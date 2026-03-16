f0574aecf3d47a88ef5bd067a82fa3e8cd60d2ae
Benyamin Shoraki <benyaminchor@gmail.com>
2026-03-16 11:49:50 +0200

    Add: Implement data models for Intel and Attack events using Pydantic; update requirements.txt for Pydantic

---
+++ modools.py
@@ -0,0 +1,56 @@
+import uuid
+from enum import Enum
+
+from pydantic import BaseModel, Field, field_validator
+from datetime import datetime
+
+
+class SignalType(str, Enum):
+    SIGINT = "SIGINT"
+    VISING = "VISINT"
+    HUMINT = "HUMINT"
+
+
+class WeaponType(str, Enum):
+    AGM_114_Hellfire = "AGM-114 Hellfire"
+    GBU_39_SDB = "GBU-39 SDB"
+    Delilah_Missile = "Delilah Missile"
+    SPICE_250 = "SPICE-250"
+    Popeye_AGM = "Popeye AGM"
+    Griffin_LGM = "Griffin LGM"
+
+
+class DamageResult(str, Enum):
+    destroyed = "destroyed"
+    damaged = "damaged"
+    no_damage = "no_damage"
+
+
+class IntelEvent(BaseModel):
+    timestamp: datetime
+    signal_id: uuid.UUID
+    entity_id: str = Field(..., min_length=7, max_length=7)
+    reported_lat: int = Field(..., min_length=-90, max_length=90)
+    reported_lon: int = Field(..., min_length=-180, max_length=180)
+    signal_type: SignalType
+    priority_level: int
+
+    @field_validator('priority_level')
+    def validate_priority(cls, v):
+        if v not in [1, 2, 3, 4, 5, 99] and v is not None:
+            raise ValueError("Priority must be between 1-5 or 99")
+        return v
+
+
+class AttackEvent(BaseModel):
+    timestamp: datetime
+    attack_id: uuid.UUID
+    entity_id: str = Field(..., min_length=7, max_length=7)
+    weapon_type: WeaponType
+
+
+class DamageEvent(BaseModel):
+    timestamp: datetime
+    attack_id: uuid.UUID
+    entity_id: str = Field(..., min_length=7, max_length=7)
+    result: DamageResult
